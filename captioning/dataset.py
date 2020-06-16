5ba869022561485daebe85a7f57d5969bf1d2104
Stanisław Morawski <malpunek@gmail.com>
2020-06-16 15:49:29 +0200

    CocoFeatureDataset

---
+++ captioning/dataset.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import h5py
+from torch.utils.data.dataset import Dataset
+
+
+class CocoFeatureCaptionDataset(Dataset):
+    def __init__(self, hdf5_path, caption_path):
+        self.hdf5_file = h5py.File(Path(hdf5_path).expanduser(), "r")
+        self.features = self.hdf5_file["features"]
+
+        from pycocotools.coco import COCO
+        self.coco = COCO(caption_path)
+        self.ids = list(sorted(self.coco.imgs.keys()))
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        img_id = self.ids[idx]
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+        anns = self.coco.loadAnns(ann_ids)
+        targets = [ann['caption'] for ann in anns]
+
+        return self.features[idx], targets
+
+    def close(self):
+        self.hdf5_file.close()

6881d6928f16db775ba0c55e20fb9730f762a2e7
Stanisław Morawski <malpunek@gmail.com>
2020-06-16 15:48:57 +0200

    Captions 2 semantic terms

--- captioning/preprocessing/__main__.py
+++ captioning/preprocessing/__main__.py
@@ -1,10 +1,12 @@
 from .extend_verbs import main as extend_verbs
 from .extract_tagged_lemmas import main as extract_lemmas
-from .transform_verbs import main as transform_verbs
 from .feature_extraction import main as extract_features
+from .map_captions import main as captions_to_semantic_terms
+from .transform_verbs import main as transform_verbs
 
 extract_lemmas()
 transform_verbs()
 extend_verbs()
+captions_to_semantic_terms()
 
 extract_features()
---
+++ captioning/preprocessing/map_captions.py
@@ -0,0 +1,63 @@
+# %%
+import json
+import logging
+from functools import partial
+from pathlib import Path
+
+from tqdm.auto import tqdm
+
+from ..config import captions_path, semantic_captions_path, word_map_path
+from ..utils import ask_overwrite
+from .extract_tagged_lemmas import caption_to_tagged_lemmas
+
+
+def caption_to_terms(caption, word_map):
+    def change_verbs(terms):
+        for term in terms:
+            if term[-4:] == "NOUN":
+                yield term
+            elif (substitute := word_map.get(term, None)) is not None:
+                yield substitute
+
+    return list(change_verbs(caption_to_tagged_lemmas(caption)))
+
+
+def term_annotation(word_map, ann):
+    res = ann.copy()
+    res["caption"] = caption_to_terms(res["caption"], word_map)
+    return res
+
+
+def main():
+    if not Path(word_map_path).exists():
+        logging.critical(
+            f"{word_map_path} does not exist. Please extract tagged lemmas first!"
+        )
+        return
+
+    if not ask_overwrite(semantic_captions_path):
+        return
+
+    with open(captions_path) as caps_f, open(word_map_path) as word_map_f:
+        caps = json.load(caps_f)
+        word_map = json.load(word_map_f)
+
+    caps['annotations'] = caps['annotations'][:1000]
+    map_annotations = partial(term_annotation, word_map)
+    terms_annotations = list(
+        tqdm(
+            map(map_annotations, caps["annotations"]),
+            total=len(caps["annotations"]),
+            desc="Mapping annotations to semantic term format",
+        )
+    )
+
+    caps["annotations"] = terms_annotations
+    with open(semantic_captions_path, "w") as f:
+        json.dump(caps, f)
+
+    return caps
+
+
+if __name__ == "__main__":
+    main()

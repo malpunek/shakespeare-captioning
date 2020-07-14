d74536b798380e15461e6d689e27aae3b53cc79f
Stanisław Morawski <malpunek@gmail.com>
2020-07-14 12:25:16 +0200

    New feature extraction

--- captioning/config.py
+++ captioning/config.py
@@ -6,9 +6,16 @@ from functools import lru_cache
 from pathlib import Path
 
 import torch
+from torchvision.transforms import transforms
+from torchvision.datasets.coco import CocoCaptions
+
+
+def lazy(func):
+    return lru_cache(maxsize=1)(func)
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--word_occurance_threshold", type=int, default=200)
+parser.add_argument("-t", "--word_occurance_threshold", type=int, default=25)
 parser.add_argument("-i", "--interactive", action="store_true")
 args = parser.parse_args()
 
@@ -19,23 +26,35 @@ gpu_num = 0
 device_str = "cpu" if not torch.cuda.is_available() else f"cuda:{gpu_num}"
 device = torch.device(device_str)
 
+first_stage = {"batch_size": 16, "learning_rate": 0.001, "epochs": 10}
+
 
 max_caption_len = 20
 
 # Paths
 mscoco_root_path = Path("/data/mscoco")
-mscoco_imgs_path = mscoco_root_path / "train2014"
-captions_path = mscoco_root_path / "annotations/captions_train2014.json"
-
 computed_root_path = Path("/data/computed_shake")
 
 if not computed_root_path.exists():
     os.makedirs(computed_root_path, exist_ok=True)
 
-features_path = computed_root_path / "extracted_features.hdf5"
-feature_ids_path = computed_root_path / "extracted_ids.npy"
+coco_train_conf = {
+    "name": "Train",
+    "imgs_root_path": mscoco_root_path / "train2014",
+    "captions_path": mscoco_root_path / "annotations/captions_train2014.json",
+    "features_path": computed_root_path / "train_features.hdf5"
+}
+
+coco_val_conf = {
+    "name": "Validation",
+    "imgs_root_path": mscoco_root_path / "val2014",
+    "captions_path": mscoco_root_path / "annotations/captions_val2014.json",
+    "features_path": computed_root_path / "val_features.hdf5"
+}
+
 
 thresh_path = computed_root_path / f"thresh_{word_occurance_threshold}"
+
 if not thresh_path.exists():
     os.makedirs(thresh_path, exist_ok=True)
 
@@ -45,7 +64,6 @@ semantic_captions_path = thresh_path / "semantic_train2014.json"
 
 plays_path = "/data/shake/merged"
 nltk_data_path = "/home/malpunek/.nltk_data"
-
 
 extended_word_map_path = thresh_path / "word_map_extended.json"
 encoded_captions_path = thresh_path / "encoded_captions_train2014.json"
@@ -57,6 +75,41 @@ interactive = args.interactive
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
 
 
+def setup_nltk():
+    import nltk  # noqa: E402
+
+    if nltk.data.path[-1] != nltk_data_path:
+        nltk.data.path.append(nltk_data_path)
+
+
+# ######## Lazy #############
+
+
+def _get_dataset(dataset_conf):
+    t = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    return CocoCaptions(
+        dataset_conf["imgs_root_path"], dataset_conf["captions_path"], transforms=t
+    )
+
+
+@lazy
+def coco_captions_train():
+    return _get_dataset(coco_train_conf), coco_train_conf
+
+
+@lazy
+def coco_captions_val():
+    return _get_dataset(coco_val_conf), coco_val_conf
+
+
+@lazy
 def get_zipped_plays_paths():
     return list(
         zip(
@@ -66,14 +119,7 @@ def get_zipped_plays_paths():
     )
 
 
-def setup_nltk():
-    import nltk  # noqa: E402
-
-    if nltk.data.path[-1] != nltk_data_path:
-        nltk.data.path.append(nltk_data_path)
-
-
-@lru_cache
+@lazy
 def load_wn():
     setup_nltk()
     from nltk.corpus import wordnet
@@ -81,7 +127,7 @@ def load_wn():
     return wordnet
 
 
-@lru_cache
+@lru_cache(maxsize=8)
 def load_nlp(model="en_core_web_lg"):
     import spacy
 
@@ -101,13 +147,16 @@ def load_nlp(model="en_core_web_lg"):
     return nlp
 
 
-@lru_cache
+@lru_cache(maxsize=8)
 def load_wn_nlp(model="en_core_web_lg"):
     from spacy_wordnet.wordnet_annotator import WordnetAnnotator
 
     nlp = load_nlp(model)
     nlp.add_pipe(WordnetAnnotator(nlp.lang), after="tagger")
     return nlp
+
+
+# ##### UTILS #####
 
 
 def lgpu():
@@ -130,12 +179,15 @@ def pick_gpu():
     global device, device_str
 
     if not interactive:
+        # Not interactive stick with the default
         device_str = "cpu" if not torch.cuda.is_available() else "cuda:0"
 
     elif not torch.cuda.is_available():
+        # Interactive but cuda not available
         device_str = "cpu"
 
     elif torch.cuda.device_count() == 1:
+        # Interactive but only 1 gpu available
         device_str = "cuda:0"
 
     else:
--- captioning/preprocessing/feature_extraction.py
+++ captioning/preprocessing/feature_extraction.py
@@ -3,55 +3,46 @@ from pathlib import Path
 import h5py
 import numpy as np
 import torch
-from torchvision.datasets.coco import CocoCaptions
-from torchvision.transforms import transforms
 from tqdm.auto import tqdm
 
-from ..config import (captions_path, device, feature_ids_path, features_path,
-                      mscoco_imgs_path)
+from ..config import coco_captions_train, coco_captions_val, device
 from ..model import FeatureExtractor
 from ..utils import ask_overwrite
 
 
-def main():
-
-    if not ask_overwrite(features_path):
-        return
-
-    hdf5_fname = Path(features_path)
-    hdf5_fname.unlink(missing_ok=True)
-
+def populate_file(f, dataset):
     model = FeatureExtractor()
     model.eval()
     model.to(device)
 
-    t = transforms.Compose(
-        [
-            transforms.Resize(256),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
+    feature_shape = (len(dataset), model.out_features)
+    features = f.create_dataset("features", feature_shape, dtype="f")
 
-    dataset = CocoCaptions(
-        Path(mscoco_imgs_path).expanduser(),
-        Path(captions_path).expanduser(),
-        transform=t,
-    )
+    ids = np.array(dataset.ids)
+    h5py_ids = f.create_dataset("ids", (len(dataset),), dtype="i")
+    h5py_ids[...] = ids
 
-    with h5py.File(features_path, "a") as f:
-        feature_shape = (len(dataset), model.out_features)
-        features = f.create_dataset("features", feature_shape, dtype="f")
+    with torch.no_grad():
+        for i, (img, _) in enumerate(tqdm(dataset, desc="Computing Features")):
+            img = img.unsqueeze(0).to(device)
+            feats = model(img).cpu().numpy()
+            features[i] = feats[0]
 
-        with torch.no_grad():
-            for i, (img, _) in enumerate(tqdm(dataset, desc="Computing Features")):
-                img = img.unsqueeze(0).to(device)
-                feats = model(img).cpu().numpy()
-                features[i] = feats[0]
 
-    img_ids = np.array(dataset.ids)
-    with open(feature_ids_path, "wb") as f:
-        np.save(f, img_ids)
+def extract(dataset, conf):
+    if not ask_overwrite(conf["features_path"]):
+        return
+
+    hdf5_fname = Path(conf["features_path"])
+    hdf5_fname.unlink(missing_ok=True)
+
+    with h5py.File(conf["features_path"], "w") as f:
+        populate_file(f, dataset)
+
+
+def main():
+    for dataset, conf in (coco_captions_train(), coco_captions_val()):
+        extract(dataset, conf)
 
 
 if __name__ == "__main__":
